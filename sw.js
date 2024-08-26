2a001ac9fdf7e3d3f3e4eaee82db232630fa7480
amazou <92152045+amine-amazou@users.noreply.github.com>
2024-08-26 22:16:14 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("assets").then((cache) => {
+            return cache.addAll([
+                "./",
+                "./script.js",
+                "./sw.js",
+                "https://cdn.jsdelivr.net/gh/amine-amazou/ns-js@1.0.1/dist/NS.min.js",
+                "./logo.png",
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => {
+            return res || fetch(event.request);
+        })
+    );
+});
+
